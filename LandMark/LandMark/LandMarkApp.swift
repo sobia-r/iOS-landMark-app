0daabc43ed132ac13d98fea31504678373964913
Sobia <sobia.rasheed@gmail.com>
2024-01-01 22:50:38 +0400

    Created xCode Project

---
+++ LandMark/LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by developer on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
