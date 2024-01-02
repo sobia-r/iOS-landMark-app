ee2dba8bffccb862dfa1916cd92d8a7e2d2067e2
Sobia <sobia.rasheed@gmail.com>
2024-01-02 21:59:19 +0400

    created a custom image view

---
+++ LandMark/LandMark/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  LandMark
+//
+//  Created by developer on 02/01/2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("mt-everest")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(
+                Circle().stroke(.white, lineWidth: 4)
+            )
+            .shadow(radius: 7)
+        
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- LandMark/LandMark/ContentView.swift
+++ LandMark/LandMark/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack (alignment:.leading){
+                Text("MOUNTAINS")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                HStack{
+                    Text("National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Fountain")
+                        .font(.subheadline)
+            }
+            
+                
+                
+                
+            
+            
         }
-        .padding()
+        .padding(15)
     }
 }
 
