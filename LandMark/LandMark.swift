0bbf46d7ba035b0f3b9f61a32a866437174691e6
Sobia <sobia.rasheed@gmail.com>
2024-01-06 23:18:05 +0400

    Added Landmark model class

---
+++ LandMark/LandMark.swift
@@ -0,0 +1,40 @@
+//
+//  LandMark.swift
+//  LandMark
+//
+//  Created by developer on 06/01/2024.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct LandMark: Hashable, Codable
+{
+    var id:Int
+    var name: String
+    var park: String
+    var state:String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+
+}
+
+
---
+++ LandMark/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  LandMark
+//
+//  Created by developer on 06/01/2024.
+//
+
+import Foundation
