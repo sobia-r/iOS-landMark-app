3630db48a4a07a22c27a191eecb47aa00f09e49d
Sobia <sobia.rasheed@gmail.com>
2024-01-09 23:19:06 +0400

    Added LandMark Row

--- LandMark/LandMark/Model/ModelData.swift
+++ LandMark/LandMark/Model/ModelData.swift
@@ -9,7 +9,7 @@
 
 import Foundation
 
-
+var landmarks: [LandMark] = load("landmarkData.json")
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
---
+++ LandMark/LandMark/Views/LandMarkRow.swift
@@ -0,0 +1,23 @@
+//
+//  LandMarkRow.swift
+//  LandMark
+//
+//  Created by developer on 09/01/2024.
+//
+
+import SwiftUI
+
+
+struct LandmarkRow: View {
+    var landmark: LandMark
+
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
