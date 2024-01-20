9cf1bb3ea44cebfbf3fd493650c5c397f6c4d8f8
Sobia Jawad <sobiajawad@192.168.1.8>
2024-01-20 21:29:21 +0400

    Added a drawing data for a badge view

---
+++ LandMark/LandMark/Model/HexagonParameters.swift
@@ -0,0 +1,18 @@
+//
+//  exagonParameters.swift
+//  LandMark
+//
+//  Created by Sobia Jawad on 20/01/2024.
+//
+
+import Foundation
+import CoreGraphics
+
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+}
