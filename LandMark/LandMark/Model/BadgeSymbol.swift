e00ee07c24fe45c23323e50969c0beed8aa0ef5e
Sobia Jawad <sobiajawad@192.168.1.7>
2024-01-23 21:43:35 +0400

    Initiated badge symbol

---
+++ LandMark/LandMark/Model/BadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeSymbol.swift
+//  LandMark
+//
+//  Created by Sobia Jawad on 23/01/2024.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeSymbol()
+}
